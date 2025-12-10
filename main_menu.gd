ca9767d44f2538720825f34d871f603719868807
psychowolf960 <119869367+psychowolf960@users.noreply.github.com>
2025-12-10 20:10:17 +0100

    psuehs

--- main_menu.gd
+++ main_menu.gd
@@ -3,7 +3,6 @@ extends Node2D
 const PLAYER_SCENE = preload("res://player/player.tscn")
 
 var peer: NodeTunnelPeer
-const WORLD = preload("uid://brdd15e2em5ax")
 
 func _ready() -> void:
 	# Create the NodeTunnelPeer
@@ -27,10 +26,7 @@ func _ready() -> void:
 	
 	# Attach room_left signal
 	peer.room_left.connect(_cleanup_room)
-	
-	var map = WORLD.instantiate()
-	self.add_child(map)
-	
+
 	# At this point, we can access the online ID that the server generated for us
 	%IDLabel.text = "Online ID: " + peer.online_id
 
